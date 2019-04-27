0ee8f5b44d1696d55a18f0c460cbcf99225e76b4
mk566 <mk566@nau.edu>
2019-04-27 12:00:19 -0700

    BeerFridge

---
+++ BeerFridge/client/src/App.js
@@ -0,0 +1,111 @@
+import React, { Component } from "react";
+import Jumbotron from "./components/Jumbotron";
+import Nav from "./components/Nav";
+import Input from "./components/Input";
+import Button from "./components/Button";
+import API from "./utils/API";
+import { RecipeList, RecipeListItem } from "./components/RecipeList";
+import { Container, Row, Col } from "./components/Grid";
+
+class App extends Component {
+  state = {
+    recipes: [],
+    recipeSearch: ""
+  };
+
+  handleInputChange = event => {
+    // Destructure the name and value properties off of event.target
+    // Update the appropriate state
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    // When the form is submitted, prevent its default behavior, get recipes update the recipes state
+    event.preventDefault();
+    API.getRecipes(this.state.recipeSearch)
+      .then(res => this.setState({ recipes: res.data }))
+      .catch(err => console.log(err));
+  };
+
+  render() {
+    return (
+      <div>
+        <Nav />
+        <Jumbotron />
+        <Container>
+          <Row>
+            <Col size="md-12">
+              <form>
+                <Container>
+                  <Row>
+                    <Col size="xs-9 sm-10">
+                      <Input
+                        name="beerSearch"
+                        value={this.state.recipeSearch}
+                        onChange={this.handleInputChange}
+                        placeholder="Beer Name"
+                      />
+                       <Input
+                        name="breweryName"
+                        value={this.state.recipeSearch}
+                        onChange={this.handleInputChange}
+                        placeholder="Search For a Brewery"
+                      />
+                       <Input
+                        name="bottleSize"
+                        value={this.state.recipeSearch}
+                        onChange={this.handleInputChange}
+                        placeholder="Bottle Size"
+                      />
+                       <Input
+                        name="Quantity"
+                        value={this.state.recipeSearch}
+                        onChange={this.handleInputChange}
+                        placeholder="Quantity"
+                      />
+                    </Col>
+                    <Col size="xs-3 sm-2">
+                      <Button
+                        onClick={this.handleFormSubmit}
+                        type="success"
+                        className="input-lg"
+                      >
+                        Add Beer
+                      </Button>
+                    </Col>
+                  </Row>
+                </Container>
+              </form>
+            </Col>
+          </Row>
+          <Row>
+            <Col size="xs-12">
+              {!this.state.recipes.length ? (
+                <h1 className="text-center">Beers in my Fridge</h1>
+              ) : (
+                <RecipeList>
+                  {this.state.recipes.map(recipe => {
+                    return (
+                      <RecipeListItem
+                        key={recipe.title}
+                        title={recipe.title}
+                        href={recipe.href}
+                        ingredients={recipe.ingredients}
+                        thumbnail={recipe.thumbnail}
+                      />
+                    );
+                  })}
+                </RecipeList>
+              )}
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ BeerFridge/client/src/components/Jumbotron/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+function Jumbotron() {
+  return (
+    <div className="jumbotron text-center">
+      <h1>Your Beer Fridge</h1>
+      <a target="_blank" rel="noopener noreferrer" href="http://www.recipepuppy.com/about/api/">
+        Powered by UrBrew
+      </a>
+    </div>
+  );
+}
+
+export default Jumbotron;
